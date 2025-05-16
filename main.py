6cc2483ae16821204732195216ed6b3bbdc9c2c7
Valentina Malkova <moonly@Valentinas-MacBook-Air.local>
2025-05-16 16:22:49 +0300

    Added Hello World script

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def print_hello():
-    for _ in range(10):
-        print("Hello, World!")
-
-print_hello()
-def vivaMechMat():
-    print("Viva Mech-mat faculty!")
-
-vivaMechMat()
+print('Hello, World!')
