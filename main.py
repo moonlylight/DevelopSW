d904905f6ee38622b5f80e58e919d13ee95b238b
Valentina Malkova <moonly@Valentinas-MacBook-Air.local>
2025-05-16 16:23:49 +0300

    Fixed vivaMechMat() output

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ def print_hello():
     for _ in range(10):
         print("Hello, World!")
 
-print_hello()git add main.py
+print_hello()
+def vivaMechMat():
+    print("Viva Mech-mat faculty!")
+
+vivaMechMat()
